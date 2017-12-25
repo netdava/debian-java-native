efb9ef0efbf3e67023f5082c6c671c9fd95115dd
Ioan Eugen Stan <eu@ieugen.ro>
2017-12-25 16:36:13 +0200

    Added system properties

--- src/main/java/helloworld/HelloApt.java
+++ src/main/java/helloworld/HelloApt.java
@@ -1,18 +1,26 @@
 package helloworld;
 
-import jnr.ffi.LibraryLoader;
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+import com.sun.jna.NativeLibrary;
+import com.sun.jna.Pointer;
 
 public class HelloApt {
 
-public static interface Configuration {
-}
+    public static void main(String[] args) {
+        System.setProperty("jna.library.path", "/usr/lib/x86_64-linux-gnu");
+        System.setProperty("jna.debug_load", "true");
 
-public static interface LibApt {
-}
+        NativeLibrary apt = NativeLibrary.getInstance("apt-pkg");
+        System.out.println(apt.getName());
+        System.out.println(apt.getFile().getAbsolutePath());
 
-public static void main(String[] args) {
-        LibApt apt = LibraryLoader.create(LibApt.class).load("apt-pkg");
+        Pointer pkgVersion = apt.getGlobalVariableAddress("pkgLibVersion");
+        System.out.println("Version is " + pkgVersion.getString(0));
+    }
 
-        apt.
-}
+    public static interface LibApt extends Library {
+        LibApt INSTANCE = (LibApt) Native.loadLibrary("apt-pkg", LibApt.class);
+    }
+
 }
